67efa93497e5ba9771b58f4ebcfc4fe461e84ce2
Martynas Skaringa <skarware@gmail.com>
2020-04-02 23:19:20 +0300

    initial commit 2

--- src/main/java/org/martynas/blogapp/controller/HomeController.java
+++ src/main/java/org/martynas/blogapp/controller/HomeController.java
@@ -2,16 +2,17 @@ package org.martynas.blogapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(Model model) {
-
-        model.addAttribute("testas", "world");
-
+    public String home(ModelMap model) {
+        model.addAttribute("testas", "vienas");
+        model.addAttribute("testas2", "du");
         return "home";
     }
 }
